ddd3f0d1e4c2f084c3719af768567d5158582e5d
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-05-15 01:08:40 +0200

    task update

---
+++ src/main/java/com/example/dashboard/employeedashboard/model/dto/TaskUpdateDTO.java
@@ -0,0 +1,9 @@
+package com.example.dashboard.employeedashboard.model.dto;
+
+import lombok.Data;
+
+@Data
+public class TaskUpdateDTO {
+    private String description;
+    private int percentage;
+}
--- src/main/java/com/example/dashboard/employeedashboard/service/TaskService.java
+++ src/main/java/com/example/dashboard/employeedashboard/service/TaskService.java
@@ -1,13 +1,18 @@
 package com.example.dashboard.employeedashboard.service;
 
 import com.example.dashboard.employeedashboard.model.Task;
+import com.example.dashboard.employeedashboard.model.dto.TaskUpdateDTO;
 import com.example.dashboard.employeedashboard.repository.TaskRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@Slf4j
 public class TaskService {
 
     @Autowired
@@ -15,5 +20,22 @@ public class TaskService {
 
     public Page<Task> getAllTasks(Pageable pageable){
         return taskRepository.findAll(pageable);
+    }
+
+    public void removeTask(int taskID){
+        taskRepository.deleteById(taskID);
+    }
+
+    public void update(int taskid, TaskUpdateDTO taskUpdated) {
+        Optional<Task> task = taskRepository.findById(taskid);
+        if(task.isPresent()){
+            task.get().setDescription(taskUpdated.getDescription());
+            task.get().setPercentage(taskUpdated.getPercentage());
+            taskRepository.save(task.get());
+            log.debug("task is updated with id " + taskid);
+        }
+        else {
+            log.warn("Task with id " + taskid + " doesn not exist");
+        }
     }
 }
