2064c99bd377bcc8cf378c37b75548d2a8d750a9
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-08-20 22:58:05 +0200

    add update task

--- backend/src/test/java/com/dashboard/TaskTests.java
+++ backend/src/test/java/com/dashboard/TaskTests.java
@@ -4,9 +4,13 @@ import com.dashboard.controller.TaskRestController;
 import com.dashboard.model.Priority;
 import com.dashboard.model.Status;
 import com.dashboard.model.Task;
+import com.dashboard.repository.TaskRepository;
 import com.dashboard.service.TaskService;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,24 +43,12 @@ public class TaskTests {
     private TestRestTemplate restTemplate;
 
     @Autowired
-    private TaskRestController bankAccountController;
+    private TaskRepository taskRepository;
 
-    @Autowired
-    private TaskService taskService;
-
-    @BeforeEach
-    public void contextLoads() throws Exception {
-        assertThat(bankAccountController).isNotNull();
-        assertThat(taskService).isNotNull();
-        Task task = new Task();
-        task.setPercentage(50);
-        task.setPriority(Priority.HIGH);
-        task.setStatus(Status.DONE);
-        task.setDescription("clean all unused branches");
-        task.setTaskname("Git Branch Cleaning");
-        taskService.deleteAllTasks();
-        taskService.addNewTask(task);
-        assertThat(taskService.getAllTasks()).hasSize(1);
+    @BeforeAll
+    public void contextLoads(){
+        generateTasks().forEach(taskRepository::save);
+        assertThat(taskRepository.count()).isEqualTo(4);
     }
 
     @Test
@@ -78,17 +70,40 @@ public class TaskTests {
 
     @Test
     public void getAllList() throws URISyntaxException {
+        // load tasks
+        beforeTask();
+
         final String baseUrl = "http://localhost:" + this.port + "/v1/api/tasks";
         URI uri = new URI(baseUrl);
         final ResponseEntity<List> tasks = this.restTemplate.getForEntity(uri, List.class);
         assertThat(tasks).isNotNull();
-        assertThat(tasks.getBody()).hasSize(1);
+        assertThat(tasks.getBody()).hasSize(4);
         Assert.assertEquals(200, tasks.getStatusCodeValue());
     }
 
     @Test
-    public void updateTask() {
+    public void updateTask() throws URISyntaxException {
 
+        beforeTask();
+
+        final String baseUrl = "http://localhost:" + this.port + "/v1/api/tasks/1";
+        URI uri = new URI(baseUrl);
+        Task task = new Task();
+        task.setPercentage(100);
+        task.setPriority(Priority.HIGH);
+        task.setStatus(Status.DONE);
+        task.setDescription("clean all unused branches");
+        task.setTaskname("Git Branch Cleaning");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-COM-PERSIST", "true");
+        HttpEntity<Task> request = new HttpEntity<>(task, headers);
+        this.restTemplate.put(uri, request);
+    }
+
+    @AfterAll
+    public void cleanTestWorkspace(){
+        taskRepository.deleteAll();
+        assertThat(taskRepository.count()).isEqualTo(0);
     }
 
     @Test
@@ -99,6 +114,43 @@ public class TaskTests {
     @Test
     public void deleteAllTask() {
 
+    }
+
+    public void beforeTask(){
+        taskRepository.deleteAll();
+        generateTasks().forEach(taskRepository::save);
+    }
+
+    public List<Task> generateTasks(){
+        Task task1 = new Task();
+        task1.setPercentage(50);
+        task1.setPriority(Priority.NORMAL);
+        task1.setStatus(Status.DONE);
+        task1.setDescription("clean all unused branches");
+        task1.setTaskname("Git Branch Cleaning");
+
+        Task task2 = new Task();
+        task2.setPercentage(10);
+        task2.setPriority(Priority.HIGH);
+        task2.setStatus(Status.NOT_READY);
+        task2.setDescription("Create new branch for ticket 11");
+        task2.setTaskname("Ticket Creation");
+
+        Task task3 = new Task();
+        task3.setPercentage(20);
+        task3.setPriority(Priority.LOW);
+        task3.setStatus(Status.NOT_READY);
+        task3.setDescription("clean all unused branches");
+        task3.setTaskname("Ticket Creation");
+
+        Task task4 = new Task();
+        task4.setPercentage(30);
+        task4.setPriority(Priority.HIGH);
+        task4.setStatus(Status.READY);
+        task4.setDescription("discuss with team members about ticket 1");
+        task4.setTaskname("Discussion about Ticket 1");
+
+        return Arrays.asList(task1, task2, task3, task4);
     }
 }
 
