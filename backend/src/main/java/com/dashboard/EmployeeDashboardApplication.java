476e41b85714449fdf5529ffec22c77007276f58
Hazar Gul Nazari <hazar.nazari@favendo.com>
2021-04-18 10:11:43 +0200

    _remove frontend

--- backend/src/main/java/com/dashboard/EmployeeDashboardApplication.java
+++ backend/src/main/java/com/dashboard/EmployeeDashboardApplication.java
@@ -9,4 +9,5 @@ public class EmployeeDashboardApplication{
     public static void main(String[] args) {
         SpringApplication.run(EmployeeDashboardApplication.class, args);
     }
+
 }
--- backend/src/main/java/com/dashboard/common/Util.java
+++
@@ -1,5 +0,0 @@
-package com.dashboard.common;
-
-public class Util {
-
-}
