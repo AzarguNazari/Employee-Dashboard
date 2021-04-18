d3aabedcb239571450cae471bbf0f6ca586e1ca4
Hazar Gul Nazari <nazariazargul@gmail.com>
2021-04-18 10:52:51 +0200

    Fix swagger (#60)
    * _upgrade open api
    
    * _refactor properties
    
    * refactor
    
    Co-authored-by: Hazar Gul Nazari <hazar.nazari@favendo.com>

--- backend/src/main/java/com/dashboard/documentation/SwaggerConfig.java
+++ backend/src/main/java/com/dashboard/documentation/SwaggerConfig.java
@@ -5,45 +5,52 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@OpenAPIDefinition
 @Component
+@OpenAPIDefinition
 public class SwaggerConfig {
+
+    @Value("${backend-version}")
+    private String version;
+
+    @Value("${openAPItitle}")
+    private String openAPItitle;
 
     @Bean
     public OpenAPI openApi(){
-        return new OpenAPI().info(new Info().title("Employee Dashboard API").version("1-SNAPSHOT").license(new License().name("Licence")));
+        return new OpenAPI().info(new Info().title(openAPItitle).version(version).license(new License().name("Licence")));
     }
 
     @Bean
     public GroupedOpenApi userGroup(){
-        return GroupedOpenApi.builder().setGroup("Users").pathsToMatch("/api/v1/users/**").build();
+        return GroupedOpenApi.builder().group("Users").pathsToMatch("/api/v1/users/**").build();
     }
 
     @Bean
     public GroupedOpenApi employeeGroup(){
-        return GroupedOpenApi.builder().setGroup("Employees").pathsToMatch("/api/v1/employees/**").build();
+        return GroupedOpenApi.builder().group("Employees").pathsToMatch("/api/v1/employees/**").build();
     }
 
     @Bean
     public GroupedOpenApi taskGroup(){
-        return GroupedOpenApi.builder().setGroup("Tasks").pathsToMatch("/api/v1/tasks/**").build();
+        return GroupedOpenApi.builder().group("Tasks").pathsToMatch("/api/v1/tasks/**").build();
     }
 
     @Bean
     public GroupedOpenApi messageGroup(){
-        return GroupedOpenApi.builder().setGroup("Messages").pathsToMatch("/api/v1/messages/**").build();
+        return GroupedOpenApi.builder().group("Messages").pathsToMatch("/api/v1/messages/**").build();
     }
 
     @Bean
     public GroupedOpenApi announcementGroup(){
-        return GroupedOpenApi.builder().setGroup("Announcements").pathsToMatch("/api/v1/announcements/**").build();
+        return GroupedOpenApi.builder().group("Announcements").pathsToMatch("/api/v1/announcements/**").build();
     }
 
     @Bean
     public GroupedOpenApi attendanceGroup(){
-        return GroupedOpenApi.builder().setGroup("Attendance").pathsToMatch("/api/v1/attendance/**").build();
+        return GroupedOpenApi.builder().group("Attendance").pathsToMatch("/api/v1/attendance/**").build();
     }
 }
