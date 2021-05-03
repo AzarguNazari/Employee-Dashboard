62bb2d799f2d1eb7aec323e26912929ab8d0e00f
Hazar Gul Nazari <hazar.nazari@favendo.com>
2021-05-03 22:01:18 +0200

    add integration test

--- src/test/java/com/dashboard/services/AnnouncementServiceTest.java
+++ src/test/java/com/dashboard/services/AnnouncementServiceTest.java
@@ -1,52 +1,63 @@
 package com.dashboard.services;
 
+import com.dashboard.models.JPA.Announcement;
 import com.dashboard.repositories.AnnouncementRepository;
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Date;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-class AnnouncementServiceTest {
+public class AnnouncementServiceTest {
 
     @Mock
     private AnnouncementRepository announcementRepository;
 
+    @InjectMocks
     private AnnouncementService announcementService;
 
-    @BeforeEach
-    public void init() {
-        announcementService = new AnnouncementService(announcementRepository);
-    }
 
-    @Test
-    void save() {
-    }
-
-    @Test
-    void delete() {
-    }
-
-    @Test
-    void update() {
-    }
-
-    @Test
-    void exist() {
-    }
+//    @Test
+//    void save() {
+//    }
+//
+//    @Test
+//    void delete() {
+//    }
+//
+//    @Test
+//    void update() {
+//    }
+//
+//    @Test
+//    void exist() {
+//    }
 
     @Test
     void getAll() {
-        Mockito.when(announcementRepository.findAll().size()).thenReturn(0);
-        assertEquals(announcementService.getAll().size(), 0);
+
+        Announcement announcement1 = new Announcement();
+        announcement1.setId(1);
+        announcement1.setCreated(Date.from(Instant.now()));
+        announcement1.setText("First announcement");
+
+        List<Announcement> announcements = Arrays.asList(announcement1);
+        when(announcementRepository.findAll()).thenReturn(announcements);
+        assertEquals(announcementService.getAll(), announcements);
     }
 
-    @Test
-    void getById() {
-    }
+//    @Test
+//    void getById() {
+//    }
 }
